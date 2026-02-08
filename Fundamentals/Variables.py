5f7bf7510d86c26f43429362c488081f085bb4f4
Shivam-devx <shivamdevx369@gmail.com>
2026-02-08 15:15:15 +0530

    Variables in Python

---
+++ Fundamentals/Variables.py
@@ -0,0 +1,31 @@
+"""
+ -> Python program to define a variables and data types in python language.
+ -> Concept : Variable store data; Python is dynamically typed 
+ -> Variable are just names poiting to objects in memory.
+ -> Common types: int, float, str, bool, complex.
+"""
+
+age = 21            # integer 
+pi = 3.1416         # float 
+name = "Shivam"     # string 
+is_student = True   # boolean 
+complex_num = 2 + 3j  # complex number
+
+print("Age: ", age)
+print("Pi: ", pi)
+print("Name: ", name)
+print("Student: ", is_student)
+print("Complex Number:", complex_num)
+
+# Write a program that stores your profile( name, age, course) and print it.
+
+Student_name = "Shivam devx"
+Student_age = 20
+Student_course = "BCA"
+
+print("\n======= Student Intoduction =======\n")
+print(f"My name is {Student_name}, I am {Student_age} years old and persuing in {Student_course}.")
+
+
+
+# Task complete and topic also complete here.  
